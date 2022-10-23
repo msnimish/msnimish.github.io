53b377bb628fb2148b19bbefc18bdf4dcd90f654
msnimish <msnimish24@gmail.com>
2022-10-23 23:39:40 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "@chakra-ui/react";
 import Chatbot from "./Components/Chatbot";
 import Dummy from "./Components/Dummy";
+import Footer from "./Components/Footer";
 import { Navbar } from "./Components/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Container maxW={"100vw"} p={"0px"}>
       <Navbar maxW={"100vw"}></Navbar>
       {/* <Dummy></Dummy> */}
+      <Footer></Footer>
       <Chatbot></Chatbot>
     </Container>
   );
